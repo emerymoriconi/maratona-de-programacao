aac74d61d5c210e7bcf83c3fc56cd4a3c3f3ddc2
Émery Freitas Moriconi <119318857+emerymoriconi@users.noreply.github.com>
2023-07-18 19:22:12 -0300

    correção questão 1021 do beecrowd

--- Python/ex.1021.py
+++ Python/ex.1021.py
@@ -1,4 +1,5 @@
 valor = float(input())
+moedas = (valor - int(valor))*100
 
 n100 = 0
 n50 = 0
@@ -13,46 +14,46 @@ m010 = 0
 m005 = 0
 m001 = 0
 
-if (valor > 100):
-    n100 = int(valor/100)
+while valor >= 100:
+    n100 += 1
+    valor -= 100
+while valor >= 50:
+    n50 += 1
+    valor -= 50
+while valor >= 20:
+    n20 += 1
+    valor -= 20
+while valor >= 10:
+    n10 += 1
+    valor -= 10
+while valor >= 5:
+    n5 += 1
+    valor -= 5
+while valor >= 2:
+    n2 += 1
+    valor -= 2
+while valor >= 1:
+    m1 += 1
+    valor -= 1
 
-if ((valor-n100*100) > 50):
-    n50 = int((valor-n100*100)/50)
-
-if ((valor-n100*100-n50*50) > 20):
-    n20 = int((valor-n100*100-n50*50)/20)
-
-if ((valor-n100*100-n50*50-n20*20) > 10):
-    n10 = int((valor-n100*100-n50*50-n20*20)/10)
-
-if ((valor-n100*100-n50*50-n20*20-n10*10) > 5):
-    n5 = int((valor-n100*100-n50*50-n20*20-n10*10)/5)
-
-if ((valor-n100*100-n50*50-n20*20-n10*10-n5*5) > 2):
-    n2 = int((valor-n100*100-n50*50-n20*20-n10*10-n5*5)/2)
-
-moedas = (valor - int(valor))*100
-
-if (moedas >= 100):
-    m1 = int(moedas/100)
-
-if ((moedas-m1*100) >= 50):
-    m050 = int((moedas-m1*100)/50)
-
-if ((moedas-m1*100-m050*50) >= 25):
-    m025 = int((moedas-m1*100-m050*50)/25)
-
-if ((moedas-m1*100-m050*50-m025*25) >= 10):
-    m010 = int((moedas-m1*100-m050*50-m025*25)/10)
-
-if ((moedas-m1*100-m050*50-m025*25-m010*10) >= 5):
-    m005 = int((moedas-m1*100-m050*50-m025*25-m010*10)/5)
-
-if ((moedas-m1*100-m050*50-m025*25-m010*10-m005*5) >= 1):
-    m001 = int((moedas-m1*100-m050*50-m025*25-m010*10-m005*5)/1)
+while moedas >= 50:
+    m050 += 1
+    moedas -= 50
+while moedas >= 25:
+    m025 += 1
+    moedas -= 25
+while moedas >= 10:
+    m010 += 1
+    moedas -= 10
+while moedas >= 5:
+    m005 += 1
+    moedas -= 5
+while moedas >= 1:
+    m001 += 1
+    moedas -= 1
                
-print('NOTA:')
-print(f'{n100} nota(s) de R$ 100.')
+print('NOTAS:')
+print(f'{n100} nota(s) de R$ 100.00')
 print(f'{n50} nota(s) de R$ 50.00')
 print(f'{n20} nota(s) de R$ 20.00')
 print(f'{n10} nota(s) de R$ 10.00')
