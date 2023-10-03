7c96076b32a9d2aa15214d47ec2971c3cf71f125
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-10-03 20:54:25 -0300

    questão 1084 do Beecrowd - certa porém não-otimizada (TLE)

--- C++/Beecrowd/ex.1084.cpp
+++ C++/Beecrowd/ex.1084.cpp
@@ -4,35 +4,39 @@ using namespace std;
 
 int main(){
     int N, D, num, j, index;
+    vector<string> respostas;
 
     do {
         cin >> N >> D;
         if(N != 0){
-            vector<int> array(N);
-            int menor = 0;
+            string array;
 
             //peguei o numero 
-            for (int i=0; i<N; i++){
-                cin >> num;
-                array.push_back(num);
-            }
+            cin >> array;
+
+            int menor = array[0] - '0';
+            index = 0;
 
             for (int i=0; i<D; i++){
-                for (j=0; j<N; j++){
-                    if (array[j] < menor){
-                        menor = array[j];
+                for (j=0; j<array.length(); j++){
+                    int aux = array[j] - '0';
+                    if (aux < menor){
+                        menor = aux;
                         index = j;
                     }
                 }
                 array.erase(array.begin() + index);
+                menor = array[0] - '0';
+                index = 0;
             }
-            while(!array.empty()){
-                cout << array.front();
-                array.erase(array.begin());
-            }
-            cout << endl;
+            
+            respostas.push_back(array);
         }
     } while (N != 0);
+
+    for (string r : respostas){
+        cout << r << endl;
+    }
     
     return 0;
 }
