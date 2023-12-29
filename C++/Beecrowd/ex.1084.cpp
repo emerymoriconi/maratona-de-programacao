3c31d54fa8dd70dc7160d8225722dc5857684ec4
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-12-29 13:00:38 -0300

    Beecrowd 1084 - Apagando e Ganhando

--- C++/Beecrowd/ex.1084.cpp
+++ C++/Beecrowd/ex.1084.cpp
@@ -1,41 +1,64 @@
 #include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define INF 100000010
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define os_type int
+#define PB push_back
+#define EB emplace_back
+#define MOD 1000000007
+#define PRIME 101 
+#define MAXN 101010
+#define MAXL 23
+#define endl '\n'
 
 using namespace std;
 
 int main(){
-    int N, D, num, j, index;
+    optimize;
+    int N, D, num;
+    stack<char> pilha;
     vector<string> respostas;
 
     do {
         cin >> N >> D;
-        if(N != 0){
-            string array;
+        if(N == 0 && D == 0) break;
+        string numFinal = "";
+        string numero;
 
-            //peguei o numero 
-            cin >> array;
+        cin >> numero;
 
-            int menor = array[0] - '0';
-            index = 0;
-
-            for (int i=0; i<D; i++){
-                for (j=0; j<array.length(); j++){
-                    int aux = array[j] - '0';
-                    if (aux < menor){
-                        menor = aux;
-                        index = j;
-                    }
-                }
-                array.erase(array.begin() + index);
-                menor = array[0] - '0';
-                index = 0;
+        for (char digito : numero){
+            while (!pilha.empty() && D > 0 && pilha.top() < digito){
+                pilha.pop();
+                D--;
             }
-            
-            respostas.push_back(array);
+            pilha.push(digito);
         }
+
+        while (D > 0){
+            pilha.pop();
+            D--;
+        }
+
+        while (!pilha.empty()){
+            numFinal = pilha.top() + numFinal;
+            pilha.pop();
+        }
+
+        respostas.push_back(numFinal);
+    
     } while (N != 0);
 
-    for (string r : respostas){
-        cout << r << endl;
+    for (string n : respostas){
+        cout << n << endl;
     }
     
     return 0;
