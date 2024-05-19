ce0078515fb37d8e343381e715e9ba561d0e1522
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2024-05-19 20:22:37 -0300

    Template bacana

--- C++/Vjudge/mfp-curso-listas/lista3/d.cpp
+++ C++/Vjudge/mfp-curso-listas/lista3/d.cpp
@@ -2,6 +2,11 @@
 #define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define endl '\n'
 
+typedef long long ll;
+
+const int MAXN = 1e5 + 10;
+const ll INF = 1e18;
+
 using namespace std;
 
 int main(){
