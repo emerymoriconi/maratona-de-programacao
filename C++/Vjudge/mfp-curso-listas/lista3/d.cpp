098bba7a3a6f2a6aa8cc0c27d5d8ba7dc8fe3907
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2024-05-19 15:28:29 -0300

    Binary Search com lower bound

--- C++/Vjudge/mfp-curso-listas/lista3/d.cpp
+++ C++/Vjudge/mfp-curso-listas/lista3/d.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
 
 using namespace std;
 
 int main(){
+    optimize;
     int n, q;
-    vector<int> v;
 
     cin >> n >> q;
+
+    vector<int> v(n);
 
     for (int i = 0; i < n; i++){
         int x;
         cin >> x;
-        v.push_back(x);
+        v[i] = x;
     }
 
     for (int i = 0; i < q; i++){
         int a;
         cin >> a;
         // quero saber se a está no vetor
-        if (find(v.begin(), v.end(), a) != v.end()){
-            cout << lower_bound(v.begin(), v.end(), a) - v.begin() << endl;
+        auto it = lower_bound(v.begin(), v.end(), a);
+        if (*it == a){
+            cout << it - v.begin() << endl;
         } else {
             cout << "-1" << endl;
         }
-
     }
 
     return 0;
