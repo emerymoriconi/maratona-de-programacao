0f775800c6a1d85542ec53ff4804d620092aebe0
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2025-03-28 17:36:04 -0300

    Dice Combinations - PD

---
+++ C++/CSES/DynamicProgramming/dice-combinations.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define pb push_back
+#define ft first
+#define sd second
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int MAXN = 1e5 + 10; // tam max pra vetores
+const ll INF = 1e18; // valor muito grande (infinito)
+const ll SMALL = -1e9; // valor muito pequeno
+const int MOD = 1e9+7; // numero primo muito utilizado como modulo
+
+int main(){
+    optimize;
+    int n;
+    cin >> n;
+
+    vector<int> solucoes(n+1);
+
+    solucoes[0] = 1;
+    solucoes[1] = 1;
+
+    for (int i = 2; i <= n; i++){
+        solucoes[i] = 0;
+        for (int x = 1; x <= 6; x++){
+            if ((i - x) >= 0){
+                solucoes[i] += solucoes[i - x];
+                solucoes[i] = solucoes[i]%MOD;
+            }
+        }
+    }
+
+    cout << solucoes[n] << endl;
+
+    return 0;
+}
